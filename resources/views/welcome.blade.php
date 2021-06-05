853d8461f83f8603fd4ddbe15952be6444f75e10
Tiago Lopes <tiago.dweb@gmail.com>
2021-06-05 13:28:17 -0300

    new title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel Test
                 </div>
 
                 <div class="links">
